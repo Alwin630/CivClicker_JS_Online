71813a34f7b6376777b7a1330899960a0e51cd84
alwin630 <alwin630@gmail.com>
2020-09-04 19:52:40 -0400

    add canvas

---
+++ civclicker.js
@@ -0,0 +1,12 @@
+// Website: https://alwin630.github.io/CivClicker_JS_Online/
+
+function setup() {
+    "use strict";
+    createCanvas(1280, 980);
+}
+
+
+function draw() {
+    background(185);
+
+}
